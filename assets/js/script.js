2c54f6f5c8bcaba522f26997fd08c4b15610ee70
Hawraa Hijazi <hawraahijazi1996@gmail.com>
2024-09-20 05:59:20 +0000

    creating event listeners to the buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
 document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByName("button");
+    let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
         button.addEventListener("click", function() {
-            if (this.getAttribute("data-type")=== "submit"){
+            if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
@@ -12,10 +15,6 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
-
-
-
-
 
 function runGame() {
 
